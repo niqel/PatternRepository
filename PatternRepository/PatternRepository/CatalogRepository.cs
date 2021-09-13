0e0e9d52d2427c9bfa264806f83c4643cc9eadfc
Niqel <33219358+niqel@users.noreply.github.com>
2021-09-13 18:38:09 -0500

    se corrige error de version y llamado a interfaces

--- PatternRepository/PatternRepository/CatalogRepository.cs
+++ PatternRepository/PatternRepository/CatalogRepository.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PatternRepository.AbstractClass;
-using PatternRepository.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
